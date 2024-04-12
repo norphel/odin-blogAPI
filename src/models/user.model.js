8624ef41ea1e99141e173b488c6331f34be8866c
Norphel <113838216+norphel@users.noreply.github.com>
2024-04-12 15:59:31 +0530

    user model

---
+++ src/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    displayName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    blogs: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Blog",
+        },
+      ],
+    },
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
