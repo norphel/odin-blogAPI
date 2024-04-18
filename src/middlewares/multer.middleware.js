10ff9ebd04e7e8fa4524441d5e9e41d7e01a41a7
Norphel <113838216+norphel@users.noreply.github.com>
2024-04-18 10:27:25 +0530

    transfer multer code to its own middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
+});
+
+export const upload = multer({ storage: storage });
